b5291eefbdd1d22cd8d27af1f1fdd9a487bbf0e6
Joel Cain <joel@eloquenza.co.uk>
2018-04-30 02:03:58 +0100

    in testing

--- FractalPlotter.py
+++ FractalPlotter.py
@@ -42,6 +42,19 @@ def fractalPlotter(fractal):
     plt.figure()
     plt.pcolormesh(fractal)
     plt.show()
+    plt.close()
 
-fractalSet = setCreator(-1, -0.5, 0.001, -0.5, 0.5, 0.001, complex(-0.56, 0.165), 200, 750, "Mandelbrot set")
-fractalPlotter(fractalSet)
+def fractalSaver(x, y, a, c):
+    for i in range(len(x)):    
+        fractalSet = setCreator(-1.5, 1.5, x[i], -1, 1, y[i], complex(-0.4, 0.6), a[i], c[i], "Julia set")
+        plt.figure()
+        plt.pcolormesh(fractalSet)
+        plt.savefig("%s_x_%s_y_%s_a_%s_c_%s.png" % (str(i), str(x[i]), str(y[i]), str(a[i]), str(c[i])))
+        plt.close()
+
+#fractalSet = setCreator(-1.5, 1.5, 0.025, -1, 1, 0.025, complex(-0.4, 0.6), 25, 20, "Julia set")
+#fractalPlotter(fractalSet)
+
+xyList = [0.5, 0.25, 0.1, 0.05, 0.025, 0.01, 0.005, 0.0025, 0.001]
+acList = [10, 14, 18, 22, 26, 30, 34, 38, 42]
+fractalSaver(xyList, xyList, acList, acList)
